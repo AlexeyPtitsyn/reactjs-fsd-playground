b69e4f647f80880fb835268232b9f8e7f98466e1
Alexey Ptitsyn <alexey.ptitsyn@gmail.com>
2026-03-09 19:08:26 +0300

    Optimized with usememo, react.memo and usecallback.

--- src/entities/task/ui/TaskCard.tsx
+++ src/entities/task/ui/TaskCard.tsx
@@ -1,7 +1,8 @@
+import React from "react";
 import type { Task } from "../model/types";
 import styles from './TaskCard.module.css';
 
-const TaskCard = ({ task, onDelete }: { task: Task; onDelete: () => void }) => {
+const TaskCard = React.memo(({ task, onDelete }: { task: Task; onDelete: () => void }) => {
   return (
     <div className={styles.TaskCard}>
       <input type="checkbox" checked={task.completed} disabled />
@@ -9,6 +10,6 @@ const TaskCard = ({ task, onDelete }: { task: Task; onDelete: () => void }) => {
       <button onClick={onDelete} className={styles.DeleteButton}>Удалить</button>
     </div>
   );
-}
+});
 
 export default TaskCard;
